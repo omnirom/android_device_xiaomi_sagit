0dedba69a5d8a07f25e132e630f229ae04bee562
Vladimir Oltean <olteanv@gmail.com>
2018-12-30 22:37:58 +0200

    sagit: extract-files: introduce fixup scripts and kang mode
    Also do the following:
    * Fix passing of arguments longer than one word, such as --section "Hello world"
    * Update the hashes and fixups for blobs kanged from bardock
    
    Change-Id: I917ed382a8c0e33ba7709151f2743a723dc50617
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>
    
    Conflicts:
    	extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -15,10 +15,22 @@
 # limitations under the License.
 #
 
+function blob_fixup() {
+    case "${1}" in
+    *)  ;;
+	esac
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+	return
+fi
+
 set -e
 
 export DEVICE=sagit
 export DEVICE_COMMON=msm8998-common
 export VENDOR=xiaomi
 
-./../../$VENDOR/$DEVICE_COMMON/extract-files.sh $@
+source "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
